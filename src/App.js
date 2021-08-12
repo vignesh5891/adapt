bdc5a8598addb44c88ab626197e38ca44308ead4
vignesh <vignesh@angleritech.com>
2021-08-12 22:05:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="adapt-app">
+      <div className="side-nav">icon</div>
+      <div className="container">
+        <div className="sidebar p-10">side bar content</div>
+        <div className="content p-10">main bar content</div>
+      </div>
     </div>
   );
 }
